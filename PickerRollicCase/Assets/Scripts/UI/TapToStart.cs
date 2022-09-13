06b8af36c729e8c5675ba43376cf46f0e7b59c62
kilicibrahim <80289487+kilicibrahim@users.noreply.github.com>
2022-09-13 06:39:15 +0300

    checkpoint.2

---
+++ PickerRollicCase/Assets/Scripts/UI/TapToStart.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TapToStart : MonoBehaviour
+{
+    public static int lvlN = 1;
+    [SerializeField] private Image clear;
+    [SerializeField] private Button button;
+    [SerializeField] private TextMeshProUGUI TTStext;
+
+    void Start()
+    {
+        button.onClick.AddListener(OnButtonClicked);
+        GameEvents.current.onFailedAttemp += RecreateTapToStart;
+        GameEvents.current.onLvlFinish += RecreateTapToStart;
+    }
+
+    private void OnButtonClicked()
+    {
+        clear.enabled = false;
+        TTStext.gameObject.SetActive(false);
+        GameEvents.current.LvlStart(lvlN);
+    }
+
+    private void RecreateTapToStart(int lvlN)
+    {
+        clear.enabled = true;
+        TTStext.gameObject.SetActive(true);
+    }
+
+    private void OnDestroy()
+    {
+        GameEvents.current.onFailedAttemp -= RecreateTapToStart;
+    }
+}
---
+++ PickerRollicCase/Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI lvlCompleteText;
+    [SerializeField] private TextMeshProUGUI lvlFailedText;
+    private void Start()
+    {
+        GameEvents.current.onLvlStart += ResetTexts;
+        GameEvents.current.onLvlFinish += LvlComplete;
+        GameEvents.current.onFailedAttemp += FailedText;
+    }
+    void LvlComplete(int lvlN)
+    {
+        lvlCompleteText.gameObject.SetActive(true);
+    }
+    void FailedText(int lvlN)
+    {
+        lvlFailedText.gameObject.SetActive(true);
+    }
+    void ResetTexts(int lvlN)
+    {
+        lvlCompleteText.gameObject.SetActive(false);
+        lvlFailedText.gameObject.SetActive(false);
+    }
+    private void OnDestroy()
+    {
+        GameEvents.current.onLvlStart += ResetTexts;
+        GameEvents.current.onLvlFinish -= LvlComplete;
+        GameEvents.current.onFailedAttemp -= FailedText;
+    }
+}
